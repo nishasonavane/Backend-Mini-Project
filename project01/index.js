bbc9c331642942d559dd56c0e120d26b142f4002
Nisha <nishasonavane0958@gmail.com>
2025-05-12 08:33:10 +0530

    project complete

--- project01/index.js
+++ project01/index.js
@@ -15,11 +15,21 @@ app.get("/",function(req,res){
     })
 
 })
+app.get("/files/:filename",function(req,res){
+   fs.readFile(`./files/${req.params.filename}`,'utf-8',function(err,filedata){
+  res.render('show',{filedata:filedata,filename:req.params.filename,filedata:filedata});
+   })
+})
+
 app.post("/create",function(req,res){
-   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details ,function(err){ })
+   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details ,function(err){ 
+
    res.redirect("/")
+   })
 
 })
+
+
 app.get("/author/:username/:age",function(req,res){
     res.send(`wlcome,${req.params.username},${req.params.age}`);
 });
