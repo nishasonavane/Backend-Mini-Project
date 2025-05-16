1604c9a7824f5c12daadb02c1d343e3167775ee3
Nisha <nishasonavane0958@gmail.com>
2025-05-16 08:15:37 +0530

    edit otion

--- project01/index.js
+++ project01/index.js
@@ -1,39 +1,45 @@
 const express = require('express')
 const app = express()
-const Path =require("path")
+const Path = require("path")
 const fs = require('fs')
+const { render } = require('ejs')
 
 //parses
 app.use(express.json())
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(Path.join(__dirname,'public')));
-app.set('view engine','ejs')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(Path.join(__dirname, 'public')));
+app.set('view engine', 'ejs')
 
-app.get("/",function(req,res){
-    fs.readdir(`./files`,function(err, files){
-          res.render("index",{files:files});
+app.get("/", function (req, res) {
+    fs.readdir(`./files`, function (err, files) {
+        res.render("index", { files: files });
     })
 
 })
-app.get("/files/:filename",function(req,res){
-   fs.readFile(`./files/${req.params.filename}`,'utf-8',function(err,filedata){
-  res.render('show',{filedata:filedata,filename:req.params.filename,filedata:filedata});
-   })
+app.get("/files/:filename", function (req, res) {
+    fs.readFile(`./files/${req.params.filename}`, 'utf-8', function (err, filedata) {
+        res.render('show', { filedata: filedata, filename: req.params.filename, filedata: filedata });
+    })
+})
+app.get("/edit/:filename", function (req, res) {
+    res.render('edit', { filename: req.params.filename });
 })
 
-app.post("/create",function(req,res){
-   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details ,function(err){ 
-
-   res.redirect("/")
-   })
-
+app.post("/edit", function (req, res) {
+    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`, function (err) {
+        res.redirect("/");
+    })
 })
 
 
-app.get("/author/:username/:age",function(req,res){
-    res.send(`wlcome,${req.params.username},${req.params.age}`);
-});
-app.listen(3000,function(){
+app.post("/create", function (req, res) {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function (err) {
+        res.redirect("/")
+    })
+
+})
+
+app.listen(3000, function () {
     console.log("project starrted ")
 
 })
